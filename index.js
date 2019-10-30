23e57e62eb2a1ba428391fe5910f6518dc7bd755
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-10-30 00:18:19 -0400

    Passing test carMaker

--- index.js
+++ index.js
@@ -276,8 +276,11 @@ const argTimesTwo = num => {
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
  */
-function carMaker( /* code here */ ) {
-    /* code here */
+function carMaker(odometer) {
+    this.odometer = odometer;
+    this.drive = distance => this.odometer += distance;
+
+    return this;
 }
 
 /// ////// END OF CHALLENGE /////////
