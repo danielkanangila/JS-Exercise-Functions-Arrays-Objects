606b1066e4f3d379b058c30b70d502d38710643b
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-10-29 21:07:59 -0400

    function makePersonObject test passing

--- index.js
+++ index.js
@@ -37,8 +37,12 @@ function exampleFunction(num1, num2) {
  *   email: "leia@leia.com",
  * }
  */
-function makePersonObject( /* code here */ ) {
-    /* code here */
+function makePersonObject(id, name, email) {
+    return {
+        id: id,
+        name: name,
+        email: email
+    };
 }
 
 /**
