46fed0f432e3d02df5feb3aa1b175561c7767f37
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-10-29 21:15:21 -0400

    function getName() test passing

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
  */
-function getName( /* code here */ ) {
-    /* code here */
+function getName({ id, name, email }) {
+    return `Hello, my name is ${name}`;
 }
 
 /**
